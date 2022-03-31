408e9d4ae45be54f6d370751442bb40fd4c65368
bijit027 <debbijit70@gmail.com>
2022-03-31 12:12:57 +0600

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -26,7 +26,7 @@ function Nav(){
             className="nav_logo" src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt="problem"
             />
             <img onClick={()=>history.push('/profile')}
-            className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" 
+            className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Click" 
             />
 
             </div>
